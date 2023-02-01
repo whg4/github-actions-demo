c053f57e35ac737d4de8fa9d0ce902c197d106ad
liuweihua <liuweihua@stary.com>
2023-02-01 14:13:04 +0800

    feat: add lint error example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  if(true) {
+    const [loading, setLoading] = useState(false);
+  }
   return (
     <div className="App">
       <header className="App-header">
